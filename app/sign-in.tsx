8fffb53d0229b8e6f29f964faea61f227cc43323
pkhaneff <nguyenminhlong1918@gmail.com>
2025-03-09 14:40:01 +0700

    intergrate Authentication

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,25 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from "@/constants/images"
 import icons from "@/constants/icons"
+import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provider'
+import { Redirect } from 'expo-router'
 
 const signIn = () => {
+  const { refetch, loading, isLoggedIn} = useGlobalContext()
 
-  const handleLogin = () => {
+  if(!loading && isLoggedIn) return <Redirect href='/'/>
 
+  const handleLogin = async () => {
+    const result = await login()
+
+    if(result){
+      refetch()
+    }else{
+      Alert.alert('Error', 'Failed to login')
+    }
   }
   return (
     <SafeAreaView className='bg-white h-full'>
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -63,7 +63,7 @@ export async function logout(){
     }
 }
 
-export async function getUser(){
+export async function getCurrentUser(){
     try {
         const response = await account.get()
         if(response.$id){
