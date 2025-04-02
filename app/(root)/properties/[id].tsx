c53f83d9899e0b48d3ff4a58c5deadc36b597d3c
pkhaneff <nguyenminhlong1918@gmail.com>
2025-04-02 08:58:53 +0700

    feat/test-ai-3

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -13,7 +13,7 @@ import { router, useLocalSearchParams } from "expo-router";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import Comment from "@/components/Comment";
-import { facilities } from "./constants/data";
+import { facilities } from "@/constants/data";
 
 import { useAppwrite } from "@/lib/useAppwrite";
 import { getPropertyById } from "@/lib/appwrite";
